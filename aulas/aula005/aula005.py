a88d1d6aa6c04c8b2d3913d625b5767c00fcbf4d
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-08-21 15:34:41 -0300

    update

---
+++ aulas/aula005/aula005.py
@@ -0,0 +1,13 @@
+# manipulando texto
+frase = 'Luccas é lindão'
+lenght = len(frase)
+print(lenght)
+qntdc = frase.count('c')
+print(qntdc)
+qntdc = frase.count('c',0,5)
+print(qntdc)
+findcass = frase.find('ccas')
+print(findcass)
+frase = frase.replace('Luccas','Gostoso')
+print(frase)
+
--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,2 +1,6 @@
 # tocando mp3
 import pygame
+pygame.init()
+pygame.mixer.music.load('ex019.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
