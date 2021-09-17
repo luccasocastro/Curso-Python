1a2f1eeab33ea51f8cffbccd12e3beae46a9c827
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-17 10:27:29 -0300

    update jokenpy

--- JoKenPy/main.py
+++ JoKenPy/main.py
@@ -1,5 +1,8 @@
 import random
 
+cont_player = 0
+cont_pc = 0
+
 while True:
 
     choices = ['pedra', 'papel', 'tesoura']
@@ -18,34 +21,49 @@ while True:
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Computador ganhooouuu!!!')
+            cont_pc += 1
         elif computer == 'tesoura':
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Jogador ganhooouuu!!!')
+            cont_player += 1
 
     elif player == 'papel':
         if computer == 'pedra':
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Jogador ganhooouuu!!!')
+            cont_player += 1
         elif computer == 'tesoura':
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Computador ganhooouuu!!!')
+            cont_pc += 1
 
     elif player == 'tesoura':
         if computer == 'pedra':
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Computador ganhooouuu!!!')
+            cont_pc += 1
         elif computer == 'papel':
             print('Computador: ', computer)
             print('Jogador: ', player)
             print('Jogador ganhooouuu!!!')
+            cont_player += 1
 
     play_again = input('Deseja jogar novamente? (sim/nao): ').lower()
 
     if play_again != "sim":
         break
 
+print('=-=-=-=-=-=-=-=-=-=-=-=-')
+print(f'Pontos computador: {cont_pc}')
+print(f'Pontos jogador: {cont_player}')
+if cont_pc > cont_player:
+    print('Computador venceu essa disputa!!!')
+elif cont_player > cont_pc:
+    print('Jogador venceu essa disputa!!!')
+else:
+    print('A disputa termina com empate!!!')
 print('Até a próxima...')
