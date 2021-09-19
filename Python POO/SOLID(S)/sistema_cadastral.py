6bb7c6d61d64885b05a761bd8baceb1d30c7c032
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-19 12:48:41 -0300

    update snakepy

--- Python POO/SOLID(S)/sistema_cadastral.py
+++
@@ -1,9 +0,0 @@
-class SistemaCadastral:
-    def cadastrar(self, nome: str, idade: int) -> None:
-        if isinstance(nome, str) and isinstance(idade, int):
-            print('Acessando o banco de dados...')
-            print('Cadastrar o usuário {}, idade {}'.format(nome, idade))
-        else:
-            print('Dados inválidos')
-
-
