77221b48cfc61e011685e7c6a5346a7f89f581bd
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-10-29 10:17:59 -0300

    update atv01 of poo

--- Jupyter/Atividade 01/main.py
+++ Jupyter/Atividade 01/main.py
@@ -1,24 +1,21 @@
-windows = 0
-linux = 0
-unix = 0
-netware = 0
-macos = 0
-outro = 0
+so = ['Windows','Unix','Linux','Netware','Mac OS','Outro']
+votos = []
 total = 0
-op = 1
-while(op != 0):
-    op = int(input('Em qual SO deseja votar?\n1 - Windows\n2 - Unix\n3 - Linux\n4 - Netware\n5 - Mac OS\n6 - Outro\nDigite 0 para sair...'))
-    
-    if op == 1:
-        windows += 1
-    elif op == 2:
-        unix += 1
-    elif op == 3:
-        linux += 1
-    elif op == 4:
-        netware += 1
-    elif op == 5:
-        macos += 1
-    elif op == 6:
-        outro += 1
+mais_votos = 0
+mais_votado = ''
 
+print('Informe a qntd de votos:')
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
+for i in range(0,6):
+    votos.append(float(input(f'{so[i]}:')))
+
+for i in range(0,6):
+    total += votos[i]
+    if votos[i] > mais_votos:
+        mais_votos = votos[i]
+        mais_votado = so[i]
+
+porc = (mais_votos * 100)/total
+
+print('Total=',total)
+print(f'O sistema com mais votos foi {mais_votado} com {mais_votos} votos, equivalentes a {porc:.1f}% dos votos')
