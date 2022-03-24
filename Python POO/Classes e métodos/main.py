fb1978ff12d95516a1b44c76bce6357ff8aacc44
Luccas Castro <emaildocastro.adm01@gmail.com>
2022-03-24 15:43:06 -0300

    update makeapi

--- Python POO/Classes e métodos/main.py
+++ Python POO/Classes e métodos/main.py
@@ -1,14 +1,8 @@
 from pessoa import Pessoa
 
-p1 = Pessoa('Luccas', 19)
+p1 = Pessoa('Luccas', 20)
 p1.comer()
 p1.parar_de_comer()
 p1.falar('computação')
 p1.parar_de_falar()
 print(p1.get_ano_nascimento())
-
-
-
-
-
-
